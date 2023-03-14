0e112d2978cc9a2a3a9e67a611ba38efc4a54c2d
BigBoiJonny <jonny98.0@outlook.com>
2023-03-14 21:24:30 +0000

    get requests

--- routes/videos.js
+++ routes/videos.js
@@ -2,8 +2,33 @@ const express = require("express");
 const fs = require("fs");
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  fs.readFile("./data/videos.json", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        message: "Could not read videos from JSON file",
+      });
+    }
+    const videos = JSON.parse(data);
+    res.json(videos);
+  });
+});
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  const videoId = req.params.id;
+  fs.readFile("./data/videos.json", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        message: "Could not read videos from JSON file",
+      });
+    }
+    const videos = JSON.parse(data);
+    const selectedVideo = videos.filter((video) => video.id === videoId);
+
+    res.json(selectedVideo);
+  });
+});
 
 module.exports = router;
