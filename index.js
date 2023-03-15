c0f4706fb778a6705c2678216a61e6f65b2e751e
BigBoiJonny <jonny98.0@outlook.com>
2023-03-15 22:16:40 +0000

    upload video

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const videos = require("./routes/videos");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
 app.use("/videos", videos);
 
--- routes/videos.js
+++ routes/videos.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
-
-router.use(express.json());
+const { v4: uuid } = require("uuid");
 
 router.get("/", (req, res) => {
   fs.readFile("./data/videos.json", (err, data) => {
@@ -15,6 +14,42 @@ router.get("/", (req, res) => {
     const videos = JSON.parse(data);
     res.json(videos);
   });
+});
+
+router.post("/", (req, res) => {
+  const { title, channel, description, image, duration } = req.body;
+  const newVideo = {
+    id: uuid(),
+    title: title,
+    channel: channel,
+    description: description,
+    imgage: image,
+    views: 0,
+    likes: 0,
+    duration: duration,
+    timestamp: new Date().getTime(),
+  };
+
+  fs.readFile("./data/videos.json", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        message: "Could not read videos from JSON file",
+      });
+    }
+    const videos = JSON.parse(data);
+    videos.push(newVideo);
+
+    fs.writeFile("./data/videos.json", JSON.stringify(videos), (err) => {
+      if (err) {
+        return res.status(500).json({
+          error: true,
+          message: "Could not write videos to JSON file",
+        });
+      }
+    });
+  });
+  res.status(201).json(newVideo);
 });
 
 router.get("/:id", (req, res) => {
